da3e0da36a7b5072838ba9b4b5fd4132f3efc54f
kim_haaga <kim.haaga@live.se>
2021-06-27 13:56:05 +0200

    Add greet me function and spec

--- implementation/specs/foo.spec.js
+++ implementation/specs/foo.spec.js
@@ -2,8 +2,19 @@ const Foo = require('../src/foo');
 const { expect } = require('chai');
 
 describe('Foo', () => {
-  const subject = new Foo('baz')
-  it('is expected to return baz', () => {
-    expect(subject.bar).to.equal('baz')
+  const subject = new Foo('baz');
+  // x in front of it as bellow makes it so that it block don't run n the test
+  xit('is expected to return baz', () => {
+    expect(subject.bar).to.equal('baz');
   });
-})
+
+  describe('.greetMe()', () => {
+    it('is expected to be bound to subject', () => {
+      expect(subject).to.respondTo('greetMe');
+    });
+
+    it('is expected to respond with "Hello Me"', () => {
+      expect(subject.greetMe()).to.equal('Hello Me');
+    });
+  });
+});
--- implementation/src/foo.js
+++ implementation/src/foo.js
@@ -5,6 +5,10 @@ class Foo {
   // Do more research in this one if there is a way to pass in 
   // arguments with new syntax
   // bar = "baz"
+
+  greetMe(){
+    return "Hello Me"
+  }
 }
 
 module.exports = Foo
